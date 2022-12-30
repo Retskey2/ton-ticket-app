593aa265a08d21792b99227626a6887975399068
RETSKEY <bashlovka96@mail.ru>
2022-12-31 00:01:25 +0300

    add qr-scanner

--- src/pages/ValidationPage/ValidationPage.tsx
+++ src/pages/ValidationPage/ValidationPage.tsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import styles from './ValidationPage.module.scss';
+import QrScan from 'react-qr-reader';
 
 export const ValidationPage = () => {
-  const [data, setData] = useState('No result');
+  const [qrscan, setQrscan] = useState('No result');
+  const handleScan = (data: string | null) => {
+    if (data) {
+      setQrscan(data);
+    }
+  };
+  const handleError = (err: string) => {
+    console.error(err);
+  };
 
   return (
     <div className={styles.wrapper}>
       <div className={styles.main_background}>
-        <div className='my-auto'></div>
+        <div className='m-auto'>
+          <QrScan facingMode='user' delay={300} onError={handleError} onScan={handleScan} style={{ height: 360, width: 440 }} />
+        </div>
+        <p>{qrscan}</p>
       </div>
     </div>
   );
