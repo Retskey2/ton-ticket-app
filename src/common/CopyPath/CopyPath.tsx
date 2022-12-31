acc3ec873a9265044e749e1e71abafa2dbe13b16
RETSKEY <bashlovka96@mail.ru>
2022-12-31 22:17:00 +0300

    micro fix

--- src/common/CopyPath/CopyPath.tsx
+++ src/common/CopyPath/CopyPath.tsx
@@ -4,13 +4,13 @@ import CopySvg from '@assets/icons/copyIcon.svg';
 export default function CopyPath() {
   const [copySuccess, setCopySuccess] = useState<string>('Copy this link');
 
-  const copyToClipBoard = async (copyMe: string) => {
+  const copyToClipBoard = async () => {
     try {
       await navigator.clipboard.writeText(location.href);
       setCopySuccess('Copied!');
       setTimeout(() => {
         setCopySuccess('Copy this link');
-      }, 2000);
+      }, 3000);
     } catch (err) {
       setCopySuccess('Failed to copy!');
     }
