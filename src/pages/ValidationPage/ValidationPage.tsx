269492d3af49868aea9afe49c0f196b14ec66ddc
RETSKEY <bashlovka96@mail.ru>
2022-12-31 13:01:56 +0300

    adding qr-code styles

--- src/pages/ValidationPage/ValidationPage.tsx
+++ src/pages/ValidationPage/ValidationPage.tsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './ValidationPage.module.scss';
 import QrScan from 'react-qr-reader';
+import { useWindowSize } from 'usehooks-ts';
 
 export const ValidationPage = () => {
   const [qrscan, setQrscan] = useState('No result');
+  const [sizeScreen, setSizeScreen] = useState(0);
+  const { width } = useWindowSize();
+
   const handleScan = (data: string | null) => {
     if (data) {
       setQrscan(data);
@@ -13,11 +17,24 @@ export const ValidationPage = () => {
     console.error(err);
   };
 
+  useEffect(() => {
+    if (width <= 280) setSizeScreen(220);
+    if (width > 281 && width <= 320) setSizeScreen(240);
+    if (width > 321 && width < 420) setSizeScreen(300);
+    if (width >= 420 && width < 720) setSizeScreen(400);
+    if (width >= 720) setSizeScreen(500);
+  }, [width]);
+
+  console.log(sizeScreen, width);
+
   return (
     <div className={styles.wrapper}>
       <div className={styles.main_background}>
         <div className='m-auto'>
-          <QrScan facingMode='user' delay={300} onError={handleError} onScan={handleScan} style={{ height: 360, width: 440 }} />
+          <div className='font-bold text-2xl mb-6'>Scan QR code</div>
+          <div className={` w-[${sizeScreen}px]`}>
+            <QrScan className='qr-image-wrapper' facingMode='environment' delay={300} onError={handleError} onScan={handleScan} />
+          </div>
         </div>
         <p>{qrscan}</p>
       </div>
