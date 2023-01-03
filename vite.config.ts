3341cad88543039b734d7227d0b14d94939421cb
RETSKEY <bashlovka96@mail.ru>
2023-01-03 04:23:03 +0300

    test vite config №2

--- vite.config.ts
+++ vite.config.ts
@@ -7,14 +7,6 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html'),
-        nested:  path.resolve(__dirname, 'NftPage/index.html'),
-      },
-    },
-    },
     server: {
       port: 3000,
     },
